0d16009c12ad2f1005e9f1fbadf38336f419038b
chefkoch <ChefkochK@protonmail.com>
2022-11-30 19:17:25 +0100

    kopf/zahl

--- Sinfull/Functions/SendMessage.cs
+++ Sinfull/Functions/SendMessage.cs
@@ -10,11 +10,21 @@ namespace Sinfull.Functions
 {
     internal class SendMessage
     {
-        public static void Send(DiscordSocketClient client, MessageEventArgs args, string content)
+        public static DiscordMessage Send(DiscordSocketClient client, MessageEventArgs args, string content, bool delete = true)
         {
             var message = client.SendMessage(args.Message.Channel.Id, content);
-            Thread.Sleep(Data.deletetimer);
-            message.Delete();
+            if (delete == true)
+            {
+                Thread.Sleep(Data.deletetimer);
+                message.Delete();
+                return message;
+            }
+            else
+            {
+                return message;
+            }
+
+            
         }
     }
 }
